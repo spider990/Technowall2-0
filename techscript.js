8cef3e7f53004d7794140850fb9e86e74a8d4b84
spider990 <85675202+spider990@users.noreply.github.com>
2022-04-01 17:13:49 +0200

    Update techscript.js

--- techscript.js
+++ techscript.js
@@ -28,3 +28,61 @@ anime({
     direction: 'alternate'
 })
 
+const button = document.querySelector('.btn');
+
+const mouseHoverAnimation = function() {
+    anime({
+        targets: '.btn',
+        borderRadius: ['50%'],
+    })
+}
+
+const mouseOutAnimation = function() {
+    anime({
+        targets: '.btn',
+        borderRadius: ['10px'],
+    })
+}
+
+
+
+button.addEventListener('mouseover', mouseHoverAnimation),
+button.addEventListener('mouseout', mouseOutAnimation);
+
+const button2 = document.querySelector('.btn2');
+
+const mouseHoverAnimation1 = function() {
+    anime({
+        targets: '.btn2',
+        borderRadius: ['50%'],
+    })
+};
+
+const mouseOutAnimation1 = function() {
+    anime({
+        targets: '.btn2',
+        borderRadius: ['10px'],
+    })
+}
+
+button2.addEventListener('mouseover', mouseHoverAnimation1)
+button2.addEventListener('mouseout', mouseOutAnimation1)
+
+const button3 = document.querySelector('.btn3');
+
+const mouseHoverAnimation2 = function() {
+    anime({
+        targets: '.btn3',
+        borderRadius: ['50%'],
+    })
+};
+
+const mouseOutAnimation2 = function() {
+    anime({
+        targets: '.btn3',
+        borderRadius: ['10px'],
+    })
+}
+
+button3.addEventListener('mouseover', mouseHoverAnimation2)
+button3.addEventListener('mouseout', mouseOutAnimation2)
