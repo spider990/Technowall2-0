4747e4ac5657f8793f755cd643a0597c58a0555d
spider990 <85675202+spider990@users.noreply.github.com>
2022-04-01 15:09:42 +0200

    Add files via upload

---
+++ techscript.js
@@ -0,0 +1,30 @@
+anime({
+    targets: '.box',
+    easing: 'easeInOutQuad',
+    width: '100%',
+    loop: true,
+    backgroundColor: '#77f777',
+    duration: 4000,
+    direction: 'alternate'
+})
+
+anime({
+    targets: '.box2',
+    easing: 'easeInOutQuad',
+    width: '100%',
+    loop: true,
+    backgroundColor: '#77f777',
+    duration: 5000,
+    direction: 'alternate'
+})
+
+anime({
+    targets: '.box3',
+    easing: 'easeInOutQuad',
+    width: '100%',
+    loop: true,
+    backgroundColor: '#77f777',
+    duration: 6000,
+    direction: 'alternate'
+})
+
